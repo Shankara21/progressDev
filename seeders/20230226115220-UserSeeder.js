8fa1e29d6d1f53a5c2ba2007f18dbaf6965200ea
Shankara21 <lazuardit21@gmail.com>
2023-02-26 18:59:24 +0700

    userSeeder

---
+++ seeders/20230226115220-UserSeeder.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert("MstUsers", [
+      {
+        username: "admin",
+        fullname: "Muhammad Lazuardi Timur",
+        email: "admin@gmail.com",
+        userLevel: "Admin",
+        password:
+          "$2a$12$nNW7oDhxFUTRa75CtSiaLuQhKw9VPKwk59xtd1YM7pZsX07Ph/TYW",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("MstUsers", null, {});
+  },
+};
