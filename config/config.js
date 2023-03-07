14a5af04312c49e75ea2693178b3750e0129a99c
Shankara21 <lazuardit21@gmail.com>
2023-03-07 13:16:20 +0700

    update config

--- config/config.js
+++ config/config.js
@@ -1,11 +1,5 @@
 require("dotenv").config();
-const {
-  DB_USERNAME,
-  DB_PASSWORD,
-  DB_HOST,
-  DB_NAME,
-  DB_DIALECT = mysql,
-} = process.env;
+const { DB_USERNAME, DB_PASSWORD, DB_HOST, DB_NAME, DB_DIALECT } = process.env;
 module.exports = {
   development: {
     username: DB_USERNAME,
@@ -15,17 +9,24 @@ module.exports = {
     dialect: DB_DIALECT,
   },
   test: {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
-    dialect: "mysql",
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    host: DB_HOST,
+    dialect: DB_DIALECT,
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    host: DB_HOST,
+    dialect: DB_DIALECT,
+  },
+  prod: {
+    username: "iot_prod",
+    password: "123456",
+    database: "progressDev",
+    host: "192.168.9.47",
     dialect: "mysql",
   },
 };
--- config/connection.js
+++ config/connection.js
@@ -8,5 +8,11 @@ const progressDev = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
   dialect: DB_DIALECT,
   logging: false,
 });
+// const progressDev = new Sequelize(
+//   config.prod.database,
+//   config.prod.username,
+//   config.prod.password,
+//   config.prod
+// );
 
 module.exports = { progressDev };
